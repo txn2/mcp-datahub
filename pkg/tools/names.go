0b1fa8aacec1a4bcd9bb7eac842ba993464d4c2e
cjimti <cj@imti.co>
2026-01-26 22:46:57 -0800

    Register `ToolGetColumnLineage` for column-level lineage and update tests

--- pkg/tools/names.go
+++ pkg/tools/names.go
@@ -9,6 +9,7 @@ const (
 	ToolGetEntity        ToolName = "datahub_get_entity"
 	ToolGetSchema        ToolName = "datahub_get_schema"
 	ToolGetLineage       ToolName = "datahub_get_lineage"
+	ToolGetColumnLineage ToolName = "datahub_get_column_lineage"
 	ToolGetQueries       ToolName = "datahub_get_queries"
 	ToolGetGlossaryTerm  ToolName = "datahub_get_glossary_term"
 	ToolListTags         ToolName = "datahub_list_tags"
@@ -25,6 +26,7 @@ func AllTools() []ToolName {
 		ToolGetEntity,
 		ToolGetSchema,
 		ToolGetLineage,
+		ToolGetColumnLineage,
 		ToolGetQueries,
 		ToolGetGlossaryTerm,
 		ToolListTags,
